47e5d727279ffbd3381f1bb71232fdaece8e51d8
miao0007 <miao188@hotmail.com>
2020-09-12 16:37:25 +0930

    add comments for each route in html-routes.js

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -2,13 +2,16 @@ const path = require("path");
 const express = require("express");
 
 module.exports = function(app) {
-    app.get("/", (_,res) => {
+    // route to he index.html page
+    app.get("/", (req,res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
-    app.get("/stats", (_,res) => {
+    // route to the stats.html page
+    app.get("/stats", (req,res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
-    app.get("/exercise?", (_, res) => {
+    // route to the exercise.html page
+    app.get("/exercise?", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 }
